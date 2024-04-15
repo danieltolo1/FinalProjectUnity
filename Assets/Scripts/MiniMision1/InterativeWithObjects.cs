2895be5df32e0a0e84a12455dfbb1ebeaf07f12c
danieltolo1 <danieltolo1@hotmail.com>
2024-04-15 16:02:29 -0500

    Mecanica Mini Mision1
    Recolectar objetos y contarlos en un 40%

---
+++ Assets/Scripts/MiniMision1/InterativeWithObjects.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InterativeWithObjects : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
