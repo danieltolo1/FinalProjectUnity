51d4b3bf75ab62f78869d2003e760a53d819b564
danieltolo1 <danieltolo1@hotmail.com>
2024-04-17 09:56:18 -0500

    Mecanica recoger objetos completa UI en 20%

--- Assets/Scripts/MiniMision1/InterativeWithObjects.cs
+++ Assets/Scripts/MiniMision1/InterativeWithObjects.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class InterativeWithObjects : MonoBehaviour
 {
@@ -8,18 +9,23 @@ public class InterativeWithObjects : MonoBehaviour
     public GameObject panel1;
     public Inventory inventory;
     public bool playerNear;
+    private TextMeshProUGUI textMesh;
     // Start is called before the first frame update
     void Start()
     {
+        textMesh = GetComponent<TextMeshProUGUI>();
         inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
     }
 
-    void Update(){
-        if (Input.GetKeyDown (KeyCode.X) && playerNear == true){
-               
-                inventory.Cantidad = inventory.Cantidad + 1;
-                Destroy(gameObject);
-             }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X) && playerNear == true)
+        {
+
+            inventory.Cantidad = inventory.Cantidad + 1;
+            Destroy(gameObject);
+            panel1.SetActive(false);
+        }
     }
 
     // Update is called once per frame
@@ -27,20 +33,28 @@ public class InterativeWithObjects : MonoBehaviour
     {
         if (other.tag == "Player")
         {
-            panel1.SetActive(true); 
+            panel1.SetActive(true);
             playerNear = true;
-        }else{
+        }
+        else
+        {
             panel1.SetActive(false);
             playerNear = false;
         }
 
     }
 
-    private void OnTriggerExit(Collider other) {
+    private void OnTriggerExit(Collider other)
+    {
         if (other.tag == "Player")
         {
-            panel1.SetActive(false); 
+            panel1.SetActive(false);
             playerNear = false;
+        }
+        else
+        {
+            panel1.SetActive(true);
+            playerNear = true;
+        }
     }
-}
 }
