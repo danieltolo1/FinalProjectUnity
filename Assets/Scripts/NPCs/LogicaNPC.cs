8f294b3126cd89f847fb718d1a11c3129132bd6a
JonathanFrancoS <jofranco087@gmail.com>
2024-04-25 09:59:45 -0500

    Update Player Cam
    Ready

--- Assets/Scripts/NPCs/LogicaNPC.cs
+++ Assets/Scripts/NPCs/LogicaNPC.cs
@@ -24,7 +24,7 @@ public class LogicaNPC : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        simboloMision.SetActive(true);
+        //simboloMision.SetActive(true);
         panelNPCStarConversation.SetActive(false);
     }
 
