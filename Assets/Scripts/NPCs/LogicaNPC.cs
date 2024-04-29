0a4c0ad16668f6bd103461fc6ef403d8a84822e5
danieltolo1 <danieltolo1@hotmail.com>
2024-04-29 13:19:24 -0500

    Mision1Completa

--- Assets/Scripts/NPCs/LogicaNPC.cs
+++ Assets/Scripts/NPCs/LogicaNPC.cs
@@ -16,10 +16,14 @@ public class LogicaNPC : MonoBehaviour
     public GameObject panelNPCConversation3;
     public Inventory inventory;
 
+    int PlayerFar = 1;
+
     public GameObject panelNPCMision;
     public TextMeshProUGUI textoMision;
     public bool jugadorCerca;
     public bool aceptarMision;
+
+    public bool finalMision;
     public GameObject botonDeMision;
 
     public TestController testController;
@@ -27,6 +31,9 @@ public class LogicaNPC : MonoBehaviour
 
     public GameObject[] ObjetosMision1;
     public GameObject[] PanelesMision1;
+    public GameObject panelFinishMision;
+    public GameObject panelCongratulationMision;
+    public GameObject obstaculesBoxes;
 
     // Start is called before the first frame update
     void Start()
@@ -34,6 +41,8 @@ public class LogicaNPC : MonoBehaviour
         simboloMision.SetActive(true);
         testController = GameObject.FindGameObjectWithTag("Player").GetComponent<TestController>();
         panelNPCStarConversation.SetActive(false);
+
+
     }
 
     // Update is called once per frame
@@ -51,15 +60,16 @@ public class LogicaNPC : MonoBehaviour
             cameraController.rotationSpeed = 0;
         }
 
-        if (inventory.Cantidad == 4)
+        if (inventory.Cantidad == 4 && PlayerFar == 1)
         {
-            Debug.Log("lOGRE LA MISION");
+            panelCongratulationMision.SetActive(true);
             for (int i = 0; i < PanelesMision1.Length; i++)
             {
                 PanelesMision1[i].SetActive(false);
             }
-
         }
+
+
     }
 
     private void OnTriggerEnter(Collider other)
@@ -67,13 +77,23 @@ public class LogicaNPC : MonoBehaviour
         if (other.tag == "Player")
         {
             jugadorCerca = true;
+            PlayerFar = 0;
 
             if (aceptarMision == false)
             {
                 panelNPCStarConversation.SetActive(true);
-
-
             }
+        }
+
+        if (inventory.Cantidad == 4 && other.tag == "Player")
+        {
+            panelCongratulationMision.SetActive(false);
+            jugadorCerca = true;
+            Debug.Log("Gane");
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+            panelFinishMision.SetActive(true);
+            inventory.Cantidad = 0;
         }
 
     }
@@ -82,6 +102,7 @@ public class LogicaNPC : MonoBehaviour
     {
         if (other.tag == "Player")
         {
+            PlayerFar = 1;
             jugadorCerca = false;
             panelNPCStarConversation.SetActive(false);
             panelNPCConversation1.SetActive(false);
@@ -138,8 +159,17 @@ public class LogicaNPC : MonoBehaviour
         {
             ObjetosMision1[i].SetActive(true);
         }
-
-
-
     }
+
+    public void FinishMision1()
+    {
+        panelFinishMision.SetActive(false);
+        simboloMision.SetActive(false);
+        obstaculesBoxes.SetActive(false);
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+
 }
